bb8e9bf5aa1ca24bad3ad8b9e9ba88bfe18fa432
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2024-07-27 12:09:14 -0400

    extract aspect ratio column

--- Scripts/Utility/extract_subtitle_labels.py
+++ Scripts/Utility/extract_subtitle_labels.py
@@ -10,6 +10,7 @@ import os
 
 # Set the CSV directory here.
 dirpath = ""
+os.makedirs(dirpath, exist_ok=True)
 # If multiple subtitle tracks, change to the one containing the labels.
 subtitle_track = 1
 
@@ -21,12 +22,16 @@ offset = timeline.GetStartFrame()
 
 filepath = os.path.join(dirpath, os.path.splitext(clipname)[0] + ".csv")
 
+width = timeline.GetSetting("timelineResolutionWidth")
+height = timeline.GetSetting("timelineResolutionHeight")
+aspect = int(width) / int(height)
+
 # Open the output file
 with open(filepath, "w", newline="") as file:
     writer = csv.writer(file)
 
     # The column names in order
-    header = ["file", "label", "start", "end", "duration"]
+    header = ["file", "label", "start", "end", "duration", "aspect_ratio"]
     writer.writerow(header)
 
     for subtitle in timeline.GetItemListInTrack("subtitle", subtitle_track):
@@ -34,4 +39,4 @@ with open(filepath, "w", newline="") as file:
         start = subtitle.GetStart() - offset
         end = subtitle.GetEnd() - offset
         duration = end - start
-        writer.writerow([clipname, name, start, end, duration])
+        writer.writerow([clipname, name, start, end, duration, aspect])
